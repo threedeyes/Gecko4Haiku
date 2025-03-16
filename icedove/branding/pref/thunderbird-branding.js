af972de9ce00e400e8f810b055b58c8413eb71e1
Gerasim Troeglazov <3dEyes@gmail.com>
2025-03-17 00:37:23 +1000

    Icedove: add patchset, branding and mozconfig

---
+++ icedove/branding/pref/thunderbird-branding.js
@@ -0,0 +1,8 @@
+
+// Default start page
+pref("mailnews.start_page.url", "about:");
+
+// start page override to load after an update
+pref("mailnews.start_page.override_url", "about:");
+
+pref("app.vendorURL", "about:");
